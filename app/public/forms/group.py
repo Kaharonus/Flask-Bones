709aec370fd284de62624ba524a0a3b68f1ec32d
Urumasi <salabapetr@email.cz>
2016-10-07 13:12:24 +0200

    Commit

--- app/public/forms/group.py
+++ app/public/forms/group.py
@@ -3,15 +3,15 @@
 import re
 from flask_wtf import Form
 from flask.ext.babel import gettext, lazy_gettext
-from wtforms import TextField, BooleanField
+from wtforms import StringField, BooleanField
 from wtforms.validators import Length, InputRequired
-from app.data.models import Group
+from app.data.models import Group, User
 from app.fields import Predicate
 
 def group_is_available(group):
     if group == "admin":
         return False
-    if not Group.if_exists(group,user.idfirm):
+    if not Group.if_exists(group,User.idfirm):
         return True
     return False
 
@@ -22,17 +22,14 @@ def safe_characters(s):
     return re.match(r'^[\w]+$', s) is not None
 
 class GroupForm(Form):
-    nazev = TextField(lazy_gettext('Group Name'), validators=[
+    nazev = StringField(lazy_gettext('Group Name'), validators=[
         Predicate(safe_characters, message=lazy_gettext("Please use only letters (a-z) and numbers")),
         Predicate(group_is_available,message=lazy_gettext("A group has already been created with that name. Try another?")),
         Length(min=2, max=30, message=lazy_gettext("Please use between 2 and 30 characters")),
         InputRequired(message=lazy_gettext("You can't leave this empty"))])
 
 class RegisterGroupForm(Form):
-    accept_tos = BooleanField(lazy_gettext('I accept the TOS'), validators=[
-        InputRequired(message=lazy_gettext("You can't leave this empty"))])
-
-    nazev = TextField(lazy_gettext('Group Name'), validators=[
+    nazev = StringField(lazy_gettext('Group Name'), validators=[
         Predicate(safe_characters, message=lazy_gettext("Please use only letters (a-z) and numbers")),
         Predicate(group_is_available,message=lazy_gettext("A group has already been created with that name. Try another?")),
         Length(min=2, max=30, message=lazy_gettext("Please use between 2 and 30 characters")),
