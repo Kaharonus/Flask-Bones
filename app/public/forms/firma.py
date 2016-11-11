55077c1e3da9913bdb3690071ae3b09210ca192b
Urumasi <salabapetr@email.cz>
2016-11-11 11:11:20 +0100

    Commit

--- app/public/forms/firma.py
+++ app/public/forms/firma.py
@@ -4,8 +4,9 @@ from flask_wtf import Form
 from flask_babel import gettext, lazy_gettext
 from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired, Length
-
+from pyvat import check_vat_number
 from app.data.models import Group, Firma
+
 
 
 class FirmaForm(Form):
@@ -21,6 +22,7 @@ class RegisterFirmaForm(FirmaForm):
     contact_person = StringField(lazy_gettext('Contact Person'), validators=[Length(max=64)])
     phone_number = StringField(lazy_gettext('Phone number'), validators=[DataRequired(lazy_gettext('This field is required')), Length(max=16)])
     website = StringField(lazy_gettext('Organization website'), validators=[Length(max=64)])
+    vatnumber = StringField(lazy_gettext('VAT number'), validators=[DataRequired(lazy_gettext('This field is required')), Length(max=32), check_vat_number])
 
     def __init__(self, *args, **kwargs):
         Form.__init__(self, *args, **kwargs)
