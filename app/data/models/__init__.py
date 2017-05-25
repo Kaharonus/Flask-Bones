497a53b4381ea4bcb92a0296368691b57f92a960
Filip Peterek <fpeterek@seznam.cz>
2017-05-25 11:39:35 +0200

    Translation

--- app/data/models/__init__.py
+++ app/data/models/__init__.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 from .group import Group
 from .user import User
-from .firma import Firma
+from .company import Company
 from .association import U_G_Association, U_F_Association, G_F_Association
 from .oauth import OAuthSignIn, Oauth
